e570fbb2a199fadcf9507a69cddd5b3bd452640b
Andy-Aseye <andyaseye@gmail.com>
2023-01-17 09:56:42 +0000

    updates

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import User from './userProfile';
 import LoginButton from './components/loginButton';
+import { useAuth0 } from '@auth0/auth0-react';
 
 function App() {
+  const { isAuthenticated } = useAuth0();
   return (
     <div className="App">
-      <LoginButton />
+      {!isAuthenticated === true?  <LoginButton /> : null}
       <User />
     </div>
   );
